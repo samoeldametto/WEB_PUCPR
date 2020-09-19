24e05ed2d9f321593a23c621073249fb4e549b10
samoeldametto <44686769+samoeldametto@users.noreply.github.com>
2020-09-19 18:54:36 -0300

    Commit - book af2 Samoel

---
+++ Book - AF2 - Samoel/target/generated-sources/annotations/tdw/entities/Book_.java
@@ -0,0 +1,18 @@
+package tdw.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-17T20:00:01")
+@StaticMetamodel(Book.class)
+public class Book_ { 
+
+    public static volatile SingularAttribute<Book, Integer> year;
+    public static volatile SingularAttribute<Book, String> author;
+    public static volatile SingularAttribute<Book, Float> price;
+    public static volatile SingularAttribute<Book, String> name;
+    public static volatile SingularAttribute<Book, Integer> id;
+    public static volatile SingularAttribute<Book, String> category;
+
+}
